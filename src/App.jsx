252cd74c472e097a85a5c95780f7a468e00427e2
san031 <98796563+san031@users.noreply.github.com>
2026-01-26 07:43:35 +0530

    Remove import of BuypassForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import './App.css'
-import BuypassForm from './Components/buypassForm'
 import {createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from '../src/Root'
 import PassValidity from './Pages/PassValidity'
